a745933516258c3368815fd791edf84cf60a5e38
HWaruguru <hannah.waruguru8@gmasil.com>
2021-09-20 22:20:58 +0300

    Add the User model

--- app/models.py
+++ app/models.py
@@ -1,8 +1,36 @@
-from app import db
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
 
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(), nullable=False)
-    email = db.Column(db.String(), unique=True, nullable=False)
-    password_hash = db.Column(db.String(), nullable=False)
+
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_hash = db.Column(db.String(255))
+    # photos = db.relationship('PhotoProfile',backref = 'user',lazy = "dynamic")
+    reviews = db.relationship('Review',backref='user',lazy="dynamic")
+
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+    
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'User {self.username}'
