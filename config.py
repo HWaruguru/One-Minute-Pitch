b0e0c95b6d19f53d832ece9bfd485d9f36077d0c
HWaruguru <hannah.waruguru8@gmasil.com>
2021-09-20 22:21:35 +0300

    Add configurations

--- config.py
+++ config.py
@@ -1,30 +1,38 @@
+import os
+
 
 class Config:
-    pass
-class Config:
-    '''
-    General configuration parent class
-    '''
-    pass
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql+psycopg2://hannahnjoroge:password@localhost/pitches"
+    )
 
+    # email configurations
+    MAIL_SERVER = "smtp.googlemail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 
 class ProdConfig(Config):
-    '''
-    Production  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
     pass
 
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql+psycopg2://hannahnjoroge:password@localhost/pitches_test"
+    )
+
+
 class DevConfig(Config):
-    '''
-    Development  configuration child class
-
-    Args:
-        Config: The parent configuration class with General configuration settings
-    '''
-
+    SQLALCHEMY_DATABASE_URI = (
+        "postgresql+psycopg2://hannahnjoroge:password@localhost/pitches"
+    )
     DEBUG = True
+
+
+config_options = {
+    "development": DevConfig,
+    "production": ProdConfig,
+    "test": TestConfig,
+}
