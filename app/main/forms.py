4ab7b15cde69d405a9b04cb81b6bf55421a6afeb
HWaruguru <hannah.waruguru8@gmasil.com>
2021-09-20 22:25:25 +0300

    Add views to main folder

---
+++ app/main/forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField
+from wtforms.validators import Required
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('comment',validators=[Required()])
+    submit = SubmitField('Submit')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators = [Required()])
+    submit = SubmitField('Submit')
