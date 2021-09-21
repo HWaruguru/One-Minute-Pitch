cd6b9a61f947c34aa01bc570492ddfeda5a9c01c
HWaruguru <hannah.waruguru8@gmasil.com>
2021-09-21 20:14:33 +0300

    Add a pitch class

--- app/models.py
+++ app/models.py
@@ -4,18 +4,22 @@ from flask_login import UserMixin
 from . import login_manager
 from datetime import datetime
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
     email = db.Column(db.String(255),unique = True,index = True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_hash = db.Column(db.String(255))
-    # photos = db.relationship('PhotoProfile',backref = 'user',lazy = "dynamic")
-    reviews = db.relationship('Review',backref='user',lazy="dynamic")
+    pitches = db.relationship('Pitch',backref='user',lazy="dynamic")
+
 
     @property
     def password(self):
@@ -23,10 +27,10 @@ class User(UserMixin,db.Model):
 
     @password.setter
     def password(self, password):
-        self.pass_secure = generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def verify_password(self,password):
-        return check_password_hash(self.pass_secure,password)
+        return check_password_hash(self.password_hash, password)
     
     def save_user(self):
         db.session.add(self)
@@ -34,3 +38,24 @@ class User(UserMixin,db.Model):
 
     def __repr__(self):
         return f'User {self.username}'
+
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+
+    id = db.Column(db.Integer,primary_key = True)
+    title= db.Column(db.String(255),index = True)
+    category = db.Column(db.String(255),unique = True,index = True)
+    pitch = db.Column(db.String(255))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return f'User {self.title}'
+
+
+
+
